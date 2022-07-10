941f2c77e133730e37aac5b330f1d1cad00736cd
Evan Merz <evanxmerz@yahoo.com>
2022-07-10 12:35:52 -0700

    Explained why I created SimpleLogger in a comment.

--- src/SimpleLogger.js
+++ src/SimpleLogger.js
@@ -1,7 +1,13 @@
 
 /*
   Really simple logging class made up just to avoid
-  a logging dependency.
+  adding a logging dependency.
+
+  As I am creating this project, I am starting to think
+  that it's not worth the trade-off to add new dependencies
+  for simple tasks. So I don't think it's worth it to add
+  a commonly used package for logging when I have no input
+  into the development or dependencies used in that package.
 */
 class SimpleLogger {
   static verbose = false;
